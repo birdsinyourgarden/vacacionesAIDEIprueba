5a3a2962cdfb99cba29fc7cae7fc8bbe2b1742b1
Ana Rueda Guiu <ana_rg95@hotmail.com>
2023-03-02 14:42:54 +0100

    laravel installation and user factory done

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'userName' => fake()->name(),
+            'userSurname' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => fake()->phoneNumber(),
+            'birthdate' => fake()->date($format = 'Y-m-d', $max = 'now'),
+            'idNumber' => fake()->idnumber(),
+            'occupation' => fake()->jobTitle(),
+            'image' => fake()->imageUrl(),
+            'startingDate' => fake()->date($format = 'Y-m-d', $max = 'now'),
+            'status' => true,
+            'isAdmin' => false,
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
